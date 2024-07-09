191a1103f59a3dc0b3f4775983c1121a3c2cc7e7
Danni Brito <danni.brito.-nd@disney.com>
2024-07-08 22:05:19 -0500

    Update Cocktail model and improve comments

--- Datafast/CoctelesApp/Example/Models/Cocktail.swift
+++ Datafast/CoctelesApp/Example/Models/Cocktail.swift
@@ -1,30 +1,32 @@
-import Combine
-import SwiftUI
 import Foundation
 
+/// Modelo para la respuesta del API que encapsula una lista de cócteles.
 struct CocktailResponse: Codable {
     let drinks: [Cocktail]
 }
 
-// Codable es un protocolo que nos ayuda a serializar (entre ellos en el formato JSON)
-// Identifiable es un protocolo que nos permite diferenciar una instancia de otra
+/// Representa un cóctel. Conforma a `Codable` para facilitar la decodificación desde JSON
+/// y a `Identifiable` para identificación única en SwiftUI Lists.
 struct Cocktail: Codable, Identifiable {
+    // 'id' es necesario para conformar a `Identifiable`. Especificamos el mapeo en `CodingKeys`.
     var id: String
     let nombre: String
     let urlImagen: String
 
-    // si los nombres que recibimos desde el API son los mismos que vamnos a usar localmente
-    // podemos saltarnos esta parte de las CodingKeys
+    /// `CodingKeys` permite mapear las propiedades del struct a las claves específicas en el JSON.
+    /// Útil cuando las claves del JSON no coinciden directamente con los nombres de las propiedades del modelo.
     enum CodingKeys: String, CodingKey {
-        case id = "idDrink"
-        case nombre = "strDrink"
-        case urlImagen = "strDrinkThumb"
+        case id = "idDrink"       // Mapea 'idDrink' del JSON a 'id' en Cocktail.
+        case nombre = "strDrink"  // Mapea 'strDrink' a 'nombre'.
+        case urlImagen = "strDrinkThumb"  // Mapea 'strDrinkThumb' a 'urlImagen'.
     }
 }
 
+// Extensión de `Cocktail` para incluir ejemplos estáticos usados en previews y testing.
 extension Cocktail {
+    /// Ejemplo estático de un cóctel para ser usado en vistas previas y pruebas.
     static let sangria: Self = .init(
-        id: UUID().uuidString,
+        id: UUID().uuidString,  // Genera un UUID aleatorio para el ID.
         nombre: "Sangría",
         urlImagen: "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg"
     )
