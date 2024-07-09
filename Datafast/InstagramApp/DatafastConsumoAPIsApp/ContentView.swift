921ebe11929a27388271d93488b68611ed799a5d
Danni Brito <danni.brito.-nd@disney.com>
2024-07-08 21:53:38 -0500

    Remove header comments, update main entry docs

--- Datafast/InstagramApp/DatafastConsumoAPIsApp/ContentView.swift
+++ Datafast/InstagramApp/DatafastConsumoAPIsApp/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  DatafastConsumoAPIsApp
-//
-//  Created by Danni Andre Brito Morales on 3/7/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- Datafast/InstagramApp/DatafastConsumoAPIsApp/DatafastConsumoAPIsAppApp.swift
+++ Datafast/InstagramApp/DatafastConsumoAPIsApp/DatafastConsumoAPIsAppApp.swift
@@ -1,17 +1,17 @@
-//
-//  DatafastConsumoAPIsAppApp.swift
-//  DatafastConsumoAPIsApp
-//
-//  Created by Danni Andre Brito Morales on 3/7/24.
-//
-
 import SwiftUI
 
+// Punto de entrada principal para la aplicación de estilo Instagram.
 @main
 struct DatafastConsumoAPIsAppApp: App {
+    // El cuerpo de una estructura de App, define la configuración de la escena de la aplicación.
     var body: some Scene {
+        // WindowGroup crea un grupo de ventanas para la interfaz de usuario.
+        // Aquí es donde se define la vista raíz que se mostrará cuando se inicie la aplicación.
         WindowGroup {
-          InstagramFeedView()
+            // InstagramFeedView es la vista principal que se muestra.
+            // Esta vista contendrá el feed principal similar al de Instagram,
+            // donde los usuarios pueden ver una lista de publicaciones.
+            InstagramFeedView()
         }
     }
 }
