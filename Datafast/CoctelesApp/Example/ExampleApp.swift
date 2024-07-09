4fe03a187f4331fd300c524758a4a3940d13aac0
Danni Brito <danni.brito.-nd@disney.com>
2024-07-08 22:06:11 -0500

    Update ExampleApp with comments and cleanup

--- Datafast/CoctelesApp/Example/ExampleApp.swift
+++ Datafast/CoctelesApp/Example/ExampleApp.swift
@@ -1,16 +1,15 @@
-//
-//  ExampleApp.swift
-//  Example
-//
-//  Created by Danni Andre Brito Morales on 1/7/24.
-//
-
 import SwiftUI
 
+// Define el punto de entrada de la aplicación SwiftUI.
 @main
 struct ExampleApp: App {
+    // El cuerpo de una estructura 'App' define la configuración de la escena principal de la aplicación.
     var body: some Scene {
+        // WindowGroup es un contenedor para una interfaz de usuario que maneja la presentación
+        // de una o más ventanas en un entorno de aplicación basada en SwiftUI.
         WindowGroup {
+            // CocktailListView es la vista raíz que se muestra al iniciar la aplicación.
+            // Desde aquí, los usuarios pueden comenzar su interacción con la funcionalidad de la aplicación.
             CocktailListView()
         }
     }
