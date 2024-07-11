af6bc1b618474794af0e25e08e8068df17156426
Danni Brito <danni.brito.-nd@disney.com>
2024-07-11 16:12:55 -0500

    New Project Initial Commit Gestures

---
+++ Datafast/Gestos/Gestos/GestosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestosApp.swift
+//  Gestos
+//
+//  Created by Danni Andre Brito Morales on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GestosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
