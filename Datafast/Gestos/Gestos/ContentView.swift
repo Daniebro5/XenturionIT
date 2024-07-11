03f11f33f1841c3e361f1a8e4be278498e5c930e
Danni Brito <danni.brito.-nd@disney.com>
2024-07-11 16:47:30 -0500

    Remove scaling and rotation gestures from ContentView

--- Datafast/Gestos/Gestos/ContentView.swift
+++ Datafast/Gestos/Gestos/ContentView.swift
@@ -1,42 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var currentAmountScale = 0.0
-  @State private var finalAmountScale = 1.0
-
-  @State private var currentAmountAngle = Angle.zero
-  @State private var finalAmountAngle = Angle.zero
 
   var body: some View {
-    VStack(spacing: 30) {
-      Text("Hola mundo - Escala!")
-        .font(.title)
-        .scaleEffect(finalAmountScale + currentAmountScale)
-        .gesture(
-          MagnifyGesture()
-            .onChanged { value in
-              currentAmountScale = value.magnification - 1
-            }
-            .onEnded{ value in
-              finalAmountScale += currentAmountScale
-              currentAmountScale = 0
-            }
-        )
-      Text("Hola mundo - Rotacion!")
-        .font(.title)
-        .rotationEffect(finalAmountAngle + currentAmountAngle)
-        .gesture(
-          RotateGesture()
-            .onChanged { value in
-              currentAmountAngle = value.rotation
-            }
-            .onEnded{ value in
-              finalAmountAngle += currentAmountAngle
-              currentAmountAngle = .zero
-            }
-        )
+    VStack {
+      Text("Hola, Mundo!")
+        .onTapGesture {
+          // el hijo siempre tendrá prioridad si los gestos tienen la misma prioridad
+          print("Text tapped")
+        }
     }
-
+    .highPriorityGesture(
+      TapGesture()
+        .onEnded{
+          print("VStack tapped")
+        }
+    )
   }
 }
 
