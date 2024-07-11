f77fd817ba384a49c02474477de53329ade7b476
Danni Brito <danni.brito.-nd@disney.com>
2024-07-11 17:59:39 -0500

    Add drag gesture handling to CardView

--- Datafast/Gestos/Gestos/CardView.swift
+++ Datafast/Gestos/Gestos/CardView.swift
@@ -30,6 +30,19 @@ struct CardView: View {
     .rotationEffect(.degrees(offset.width / 5.0))
     .offset(x: offset.width * 5)
     .opacity(2 - Double(abs(offset.width / 50)))
+    .gesture(
+      DragGesture()
+        .onChanged { gesture in
+          offset = gesture.translation
+        }
+        .onEnded{ _ in
+          if abs(offset.width) > 100 {
+            // remover la carta
+          } else {
+            offset = .zero
+          }
+        }
+    )
     .onTapGesture {
       isShowingAnswer.toggle()
     }
