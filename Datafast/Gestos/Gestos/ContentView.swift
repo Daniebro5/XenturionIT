d61498ef8206650d9f0712b208cc27962adf5305
Danni Brito <danni.brito.-nd@disney.com>
2024-07-11 16:59:00 -0500

    Update ContentView with drag gestures

--- Datafast/Gestos/Gestos/ContentView.swift
+++ Datafast/Gestos/Gestos/ContentView.swift
@@ -2,20 +2,41 @@ import SwiftUI
 
 struct ContentView: View {
 
+  // cuanto han movido el circulo
+  @State private var offset = CGSize.zero
+
+  // se está moviendo?
+  @State private var isDragging = false
+
   var body: some View {
-    VStack {
-      Text("Hola, Mundo!")
-        .onTapGesture {
-          // el hijo siempre tendrá prioridad si los gestos tienen la misma prioridad
-          print("Text tapped")
+
+    // un gesto de movimiento que actualiza el offset y la variable isDragging cuando se mueve
+    let dragGesture = DragGesture()
+      .onChanged { value in offset = value.translation }
+      .onEnded { _ in
+        withAnimation {
+          offset = .zero
+          isDragging = false
         }
-    }
-    .highPriorityGesture(
-      TapGesture()
-        .onEnded{
-          print("VStack tapped")
+      }
+
+    // gesto que activa el movimiento
+    let pressGesture = LongPressGesture()
+      .onEnded { value in
+        withAnimation {
+          isDragging = true
         }
-    )
+      }
+
+    // un gesto combinado q fuerza al usuario a un longPress y luego a mover
+    let combined = pressGesture.sequenced(before: dragGesture)
+
+    Circle()
+      .fill(.red)
+      .frame(width: 64, height: 64)
+      .scaleEffect(isDragging ? 1.5 : 1)
+      .offset(offset)
+      .gesture(combined)
   }
 }
 
