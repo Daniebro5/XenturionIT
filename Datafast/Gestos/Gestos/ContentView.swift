c660a7705c814a2b57ff3195db3feb6aeebf1219
Danni Brito <danni.brito.-nd@disney.com>
2024-07-11 16:41:37 -0500

    Add scaling and rotation gestures to ContentView

--- Datafast/Gestos/Gestos/ContentView.swift
+++ Datafast/Gestos/Gestos/ContentView.swift
@@ -1,14 +1,42 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var currentAmountScale = 0.0
+  @State private var finalAmountScale = 1.0
+
+  @State private var currentAmountAngle = Angle.zero
+  @State private var finalAmountAngle = Angle.zero
+
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    VStack(spacing: 30) {
+      Text("Hola mundo - Escala!")
+        .font(.title)
+        .scaleEffect(finalAmountScale + currentAmountScale)
+        .gesture(
+          MagnifyGesture()
+            .onChanged { value in
+              currentAmountScale = value.magnification - 1
+            }
+            .onEnded{ value in
+              finalAmountScale += currentAmountScale
+              currentAmountScale = 0
+            }
+        )
+      Text("Hola mundo - Rotacion!")
+        .font(.title)
+        .rotationEffect(finalAmountAngle + currentAmountAngle)
+        .gesture(
+          RotateGesture()
+            .onChanged { value in
+              currentAmountAngle = value.rotation
+            }
+            .onEnded{ value in
+              finalAmountAngle += currentAmountAngle
+              currentAmountAngle = .zero
+            }
+        )
     }
-    .padding()
+
   }
 }
 
