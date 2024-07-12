a65ad9f9a210de9d430dc71f27922405d18a8895
Danni Brito <danni.brito.-nd@disney.com>
2024-07-12 17:15:46 -0500

    Add initial project structure for Bookworm app

---
+++ Datafast/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Danni Andre Brito Morales on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
