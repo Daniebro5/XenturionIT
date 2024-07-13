1cc8ef9bb2b17f652b12e62a673a0b9ddb2e9e2c
Danni Brito <danni.brito.-nd@disney.com>
2024-07-13 09:10:42 -0500

    Add initial project setup for HotProspects

---
+++ Datafast/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Danni Andre Brito Morales on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
