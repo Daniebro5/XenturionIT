12a744fce534990db9501e7772fee31ceda86c02
Danni Brito <danni.brito.-nd@disney.com>
2024-07-13 10:01:44 -0500

    Add notification support to ContentView

--- Datafast/HotProspects/HotProspects/ContentView.swift
+++ Datafast/HotProspects/HotProspects/ContentView.swift
@@ -1,3 +1,4 @@
+import UserNotifications
 import SwiftUI
 
 struct ContentView: View {
@@ -6,25 +7,33 @@ struct ContentView: View {
 
   var body: some View {
     VStack {
-      Text("Hello, World!")
-        .padding()
-        .background(backgroundColor)
-
-      Text("Change Color")
-        .padding()
-        .contextMenu {
-          Button("Rojo") {
-            backgroundColor = .red
-          }
-
-          Button("Verde") {
-            backgroundColor = .green
-          }
-
-          Button("Azul") {
-            backgroundColor = .blue
+      Button("Pedir permisos") {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+          if success {
+            print("Lo logré")
+          } else if let error {
+            print(error.localizedDescription)
           }
         }
+      }
+
+      Button("Programar notificacion") {
+        let content = UNMutableNotificationContent()
+        content.title = "Colgar la ropa"
+        content.subtitle = "Si no está lloviendo"
+        content.sound = UNNotificationSound.default
+
+        // mostraremos la notificacion despues de 5 segundos que se crea
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5,
+                                                        repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                            content: content,
+                                            trigger: trigger)
+
+        // añadimos nuestra peticion
+        UNUserNotificationCenter.current().add(request)
+      }
     }
   }
 }
