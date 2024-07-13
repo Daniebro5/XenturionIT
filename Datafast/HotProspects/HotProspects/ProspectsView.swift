1f9e19649048c715d816b08d89e9d09f6a968730
Danni Brito <danni.brito.-nd@disney.com>
2024-07-13 10:46:30 -0500

    Add filter init and update UI in ProspectsView

--- Datafast/HotProspects/HotProspects/ProspectsView.swift
+++ Datafast/HotProspects/HotProspects/ProspectsView.swift
@@ -12,21 +12,40 @@ struct ProspectsView: View {
   @Environment(\.modelContext) var modelContext
   @Query(sort: \Prospect.name) var prospects: [Prospect]
 
-  var body: some View {
-    NavigationStack {
-      Text("Personas: \(prospects.count)")
-        .navigationTitle(title)
-        .toolbar {
-          Button("Scan", systemImage: "qrcode.viewfinder") {
-            let prospect = Prospect(name: "Danni André",
-                                    emailAddress: "danni.andre@icloud.com",
-                                    isContacted: false)
-            modelContext.insert(prospect)
-          }
-        }
+  init(filter: FilterType) {
+    self.filter = filter
+
+    if filter != .none {
+      let showContactedOnly = filter == .contacted
+
+      _prospects = Query(filter: #Predicate { $0.isContacted == showContactedOnly },
+                         sort: [SortDescriptor(\Prospect.name)])
     }
   }
 
+  var body: some View {
+    NavigationStack {
+      List(prospects) { prospect in
+        VStack(alignment: .leading) {
+          Text(prospect.name)
+            .font(.headline)
+          Text(prospect.emailAddress)
+            .foregroundStyle(.secondary)
+        }
+      }
+      .navigationTitle(title)
+      .toolbar {
+        Button("Scan", systemImage: "qrcode.viewfinder") {
+          let prospect = Prospect(name: "Danni André",
+                                  emailAddress: "danni.andre@icloud.com",
+                                  isContacted: false)
+          modelContext.insert(prospect)
+        }
+      }
+    }
+  }
+
+  // variable computada
   var title: String {
     switch filter {
     case .none:
