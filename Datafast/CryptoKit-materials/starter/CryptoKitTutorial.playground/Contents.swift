db4439afb6f64fb0344271b6c47c82e2dbd5ce9f
Danni Brito <danni.brito.-nd@disney.com>
2024-07-15 16:38:45 -0500

    Add data protection comments and update hash function

--- Datafast/CryptoKit-materials/starter/CryptoKitTutorial.playground/Contents.swift
+++ Datafast/CryptoKit-materials/starter/CryptoKitTutorial.playground/Contents.swift
@@ -4,6 +4,24 @@
 import UIKit
 import CryptoKit
 
+// Criptografia no solo es para uso no autorizado, verificar la corrupcion de datos.
+
+// 3 estados de proteccion de datos
+
+// datos en movimiento (envio y recibo de llamadas)
+//    TLS Transport Layer Security (1.3)
+
+// datos en uso (estan en memoria)
+//    Autenticacion de usuario, LocalAuthentication (FaceID, TouchID)
+
+// datos en descanso (almacenados en disco que no estan siendo usados)
+//    DataProtection API -> CryptoKit -> Keychain
+//    Siempre q el usuario ya tenga puesto un codigo de desbloqueo al iPhone, ya está disponible para nosotros usarlo, tipos de proteccion de archivos:
+//          ProtectedUntilFirstUserAuthentication - Protegido hasta la autenticacion - archivos son cifrados hasta que el usuario desbloquea por primera vez el iphone (desde que esta apagado) Defecto
+//          Complete - el archivo solo estara disponible si el dispositivo esta desbloqueado
+//          CompleteUnlessOpen - si el archivo esta abierto se mantiene abierto asi se haya bloqueado
+
+
 func getData(for item: String, of type: String) -> Data {
   let filePath = Bundle.main.path(forResource: item, ofType: type)!
   return FileManager.default.contents(atPath: filePath)!
@@ -16,6 +34,20 @@ func hashItem(item: String) -> Int {
   return hasher.finalize()
 }
 
+// es una funcion unidireccional
+// debe garantizar un nivel aceptable de unicidad () que sean unicos (digest)
+// debe ser caotico (al cambiar una minima porcion de los datos asi sea un solo bit, el digest debe ser completamente diferente)
+// debe ser estable
+
+// DATA() -> (HASH) = Digest // si es posible
+// Digest -> (X) = DATA() // Imposible
+
+// aaaaaaaaaaaaaaaab -> (HASH) = 123456789
+// aaaaaaaaaaaaaaaaa -> (HASH) = 168652386
+// aaaaaaaaaaaaaaaaa -> (HASH) = 168652386
+// aaaaaaaaaaaaaaaaa -> (HASH) = 168652386
+// aaaaaaaaaaaaaaaaa -> (HASH) = 168652386
+// aaaaaaaaaaaaaaaaa -> (HASH) = 168652386
 
 //: ### Cryptographic Hashing
 let data = getData(for: "Baby", of: "png")
@@ -33,7 +65,7 @@ UIImage(data: data)
 
 
 // Small change in `data` produces completely different digest:
-String(describing: SHA256.hash(data: "Harry is a horcrux".data(using: .utf8)!))
+String(describing: SHA512.hash(data: "Harry is a horcrux".data(using: .utf8)!))
 String(describing: SHA256.hash(data: "Harry is a horcrux".data(using: .utf8)!))
 //: ## HMAC: Hash-based Message Authentication Code
 //: Use a symmetric cryptographic key when creating the digest
