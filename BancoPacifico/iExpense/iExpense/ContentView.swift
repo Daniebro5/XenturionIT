7b1b17779f214ef734bfac844d464f08b5595181
Danni Brito <danni.brito.-nd@disney.com>
2024-08-06 09:22:47 -0500

    Update ContentView with User struct and state

--- BancoPacifico/iExpense/iExpense/ContentView.swift
+++ BancoPacifico/iExpense/iExpense/ContentView.swift
@@ -1,12 +1,23 @@
 import SwiftUI
 
+// tipo valor, si es constante ninguna propiedad cambia
+struct User {
+    var primerNombre = "Danni"
+    var apellido = "Brito"
+}
+
 struct ContentView: View {
+
+    // State escucha los cambios dentro de la propiedad y redibuja la vista cada que haya un cambio
+    @State private var user = User()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tu nombre es \(user.primerNombre) \(user.apellido)")
+
+            TextField("Primer Nombre", text: $user.primerNombre)
+
+            TextField("Apellido", text: $user.apellido)
         }
         .padding()
     }
