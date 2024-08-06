299a944766312bd04bd5d6bd3d86b09704081a58
Danni Brito <danni.brito.-nd@disney.com>
2024-08-06 10:20:55 -0500

    Update ContentView with sheet and SecondView

--- BancoPacifico/Clase2.playground/Contents.swift
+++ BancoPacifico/Clase2.playground/Contents.swift
@@ -5,3 +5,35 @@ class User {
     var firstName = "Danni"
     var lastName = "Brito"
 }
+
+
+// Patron de diseño observable
+// ReactNative, React, RxJS, RxSwift, RxKotlin
+
+// inherentemente una app mobil es una app que reacciona a eventos:
+
+// recibimos info del servidor
+// el usuario hace un tap
+// el usuario hace scroll
+// recibimos una notificacion
+// nos quedamos sin internet
+
+// reaccionamos a eventos que suceden en cualquier momento
+
+// al conjunto de toda la info (info que recibi del servidor, notificacion, estado de la red, si el usuario esta tocando la pantalla o no) lo llamamos ESTADO
+
+// y la vista simplemente es un reflejo del ESTADO
+
+
+
+// una app solo termina con un error (X) o en completitud (|)
+// T                |------------------------------------------------->
+// Tap              |------a--------------b-----------c----->
+// Notificaciones   |--1------------2-------------3--------->
+// Red              |--------------------------X---------->
+// info del server  |---1------------------4--------------->
+// mostrando modal  |------V--------------F-------------------->
+
+// Streams
+
+// Pantalla `       |--NS--M--------N-----MS---E--N---T---------->
--- BancoPacifico/iExpense/iExpense/ContentView.swift
+++ BancoPacifico/iExpense/iExpense/ContentView.swift
@@ -1,31 +1,28 @@
-import Combine
 import SwiftUI
 
-// tipo referencia, podemos compartir el estado en varias partes de la app
-// primero poner estructura, si necesitamos: herencia, referencias (compartir estado), sobrecarga; solo ahí refactorizar al uso de clases
-@Observable
-class User {
-    var primerNombre = "Danni"
-    var apellido = "Brito"
-}
-
 struct ContentView: View {
-
-    // State escucha los cambios dentro de la propiedad y redibuja la vista cada que haya un cambio
-    @State private var user = User()
+    @State private var showingSheet = false
 
     var body: some View {
-        VStack {
-            Text("Tu nombre es \(user.primerNombre) \(user.apellido)")
-
-            TextField("Primer Nombre", text: $user.primerNombre)
-
-            TextField("Apellido", text: $user.apellido)
+        Button("Show sheet") {
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet) {
+            SecondView()
+        }
     }
 }
 
-#Preview {
+struct SecondView: View {
+    var body: some View {
+        Text("Second View")
+    }
+}
+
+#Preview("ContentView") {
     ContentView()
+}
+
+#Preview("SecondView") {
+    SecondView()
 }
