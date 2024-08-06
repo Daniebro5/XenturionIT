072b963e778b1f45e45bc53b35163c4c86b3bed0
Danni Brito <danni.brito.-nd@disney.com>
2024-08-06 12:07:40 -0500

    Update AddView and ContentView, fix ExpenseItem decoding

--- BancoPacifico/iExpense/iExpense/ContentView.swift
+++ BancoPacifico/iExpense/iExpense/ContentView.swift
@@ -9,7 +9,17 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type.rawValue)
+                        }
+
+                        Spacer()
+                        Text(item.amount,
+                             format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
