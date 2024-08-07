a6299e61e01bceda543cddfc84e3dc36131494b3
Danni Brito <danni.brito.-nd@disney.com>
2024-08-07 10:41:26 -0500

    Update ContentView with async data fetch and display

--- BancoPacifico/HotProspects/HotProspects/ContentView.swift
+++ BancoPacifico/HotProspects/HotProspects/ContentView.swift
@@ -2,21 +2,30 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var selectedTab = "Uno"
+    @State private var output = ""
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Show Tab 2") {
-                selectedTab = "Dos"
+        Text(output)
+        // ejecuta codigo en otro hilo de ejecucion apenas aparezca la vista
+            .task {
+                // await quiere decir que la funcion llamada se encuentra en un contexto asincrono
+                // el hilo principal siempre esta reservado para tareas de UI
+                await fetchReadings()
             }
-            .tabItem {
-                Label("Uno", systemImage: "star")
-            }
-            Text("Tab 2")
-                .tabItem {
-                    Label("Dos", systemImage: "circle")
-                }
-                .tag("Dos")
+    }
+
+    func fetchReadings() async {
+        do {
+            // Creamos la URL
+            let url = URL(string: "https://hws.dev/readings.json")!
+            // Obtenemos la data con el URLSession
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // Decodificamos
+            let readings = try JSONDecoder().decode([Double].self,
+                                                    from: data)
+            output = "He encontrado \(readings.count) lecturas"
+        } catch {
+            print("Error de descarga")
         }
     }
 }
