409bbf629034e4248a4bee0abd2dd9177a74fddb
Danni Brito <danni.brito.-nd@disney.com>
2024-08-08 10:08:33 -0500

    Update ContentView with NavigationPath and toolbar

--- BancoPacifico/Navigation/Navigation/ContentView.swift
+++ BancoPacifico/Navigation/Navigation/ContentView.swift
@@ -2,26 +2,27 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
 
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Button("Show 32 then 64 then 32") {
-                    path = [32, 64, 32]
+            Text("Mi 4ta clase de iOS")
+                .toolbar {
+                    Button("Push 556") {
+                        path.append(556)
+                    }
+                    Button("Push Hello") {
+                        path.append("Hello")
+                    }
                 }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
-            }
+                .navigationDestination(for: Int.self) { destination in
+                    Text("You are in \(destination)")
+                }
+                .navigationDestination(for: String.self) { destination in
+                    Text("You are in \(destination)")
+                }
         }
     }
-}
-
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
 }
 
 // Hashable
