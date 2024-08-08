23b378a6ee91aa3273a8d950450fa475d42aca48
Danni Brito <danni.brito.-nd@disney.com>
2024-08-08 11:32:12 -0500

    Remove PathStore and refactor ContentView navigation

--- BancoPacifico/Navigation/Navigation/ContentView.swift
+++ BancoPacifico/Navigation/Navigation/ContentView.swift
@@ -2,37 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @ObservedObject private var pathStore = PathStore()
-
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path)
-                }
+        NavigationStack {
+            List(0 ..< 100) { i in
+                Text("Row \(i)")
+                    .font(.title)
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
+
 }
-
-struct DetailView: View {
-
-    var number: Int
-    @Binding var path: NavigationPath
-
-    var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1 ... 1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
-    }
-}
-
-
-
-// Hashable
 
 #Preview {
     ContentView()
