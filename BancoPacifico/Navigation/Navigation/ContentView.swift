b13dfbfa36751ed48b0fef654a65bf65ba3ba270
Danni Brito <danni.brito.-nd@disney.com>
2024-08-08 11:09:53 -0500

    Refactor NavigationStack to use PathStore

--- BancoPacifico/Navigation/Navigation/ContentView.swift
+++ BancoPacifico/Navigation/Navigation/ContentView.swift
@@ -2,32 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var path = [Int]()
+    @ObservedObject private var pathStore = PathStore()
 
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i, path: $pathStore.path)
                 }
         }
-
-        // path.removeAll()
-        // path = NavigationPath()
     }
 }
 
 struct DetailView: View {
 
     var number: Int
-    @Binding var path: [Int]
+    @Binding var path: NavigationPath
 
     var body: some View {
         NavigationLink("Go to Random Number", value: Int.random(in: 1 ... 1000))
             .navigationTitle("Number: \(number)")
             .toolbar {
                 Button("Home") {
-                    path.removeAll()
+                    path = NavigationPath()
                 }
             }
     }
--- BancoPacifico/Navigation/Navigation/PathStore.swift
+++ BancoPacifico/Navigation/Navigation/PathStore.swift
@@ -1,7 +1,8 @@
 import Foundation
+import SwiftUI
 
 final class PathStore: ObservableObject {
-    var path: [Int] {
+    @Published var path: NavigationPath {
         didSet {
             save()
         }
@@ -14,19 +15,20 @@ final class PathStore: ObservableObject {
     init() {
         // try? ignora el error y me envía nil si hay uno
         if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
-                path = decoded
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
                 return
             }
         }
 
         // no existió un path guardado así que lo iniciamos vacío
-        path = []
+        path = NavigationPath()
     }
 
     func save() {
+        guard let representation = path.codable else { return }
         do {
-            let data = try JSONEncoder().encode(path)
+            let data = try JSONEncoder().encode(representation)
             try data.write(to: savePath)
         } catch {
             print("Failed to save navigation data, \(error.localizedDescription)")
