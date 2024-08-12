d3a103fb63a9ec7ee9542167a59f1e52beade7ea
Danni Brito <danni.brito.-nd@disney.com>
2024-08-12 11:49:45 -0500

    Add focus state and update UI in ContentView

--- BancoPacifico/WeSplit/WeSplit/ContentView.swift
+++ BancoPacifico/WeSplit/WeSplit/ContentView.swift
@@ -7,6 +7,10 @@ struct ContentView: View {
     @State private var numeroDePersonas = 2
     @State private var propina = 20
 
+    // propiedades booleanas tratemos de usar verbos de si y no
+    // esta? es? usa? tiene?
+    @FocusState private var estaElFocusEnValorTotal: Bool
+
     let propinas = [10, 15, 20, 25, 0]
 
     // propiedad computada para el valor total
@@ -23,42 +27,53 @@ struct ContentView: View {
     }
 
     var body: some View {
-        Form {
-            Section {
-                // placeholder
-                TextField("Valor de la cuenta",
-                          // two way binding
-                          value: $valorTotal,
-                          // controla el formato de ingreso
-                          format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                )
-                .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    // placeholder
+                    TextField("Valor de la cuenta",
+                              // two way binding
+                              value: $valorTotal,
+                              // controla el formato de ingreso
+                              format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($estaElFocusEnValorTotal)
 
-                Picker("Numero de Personas",
-                       selection: $numeroDePersonas) {
-                    ForEach(2 ..< 100) {
-                        Text("\($0) personas")
-                    }
+                    Picker("Numero de Personas",
+                           selection: $numeroDePersonas) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) personas")
+                        }
 
-                    // valor    2-3-4-5-......
-                    // posicion 0-1-2-3-4-5-6-
-                }
-            }
-
-            Section {
-                Text("Cuanta propina deseas dejar?")
-                Picker("Porcentaje de propina",
-                       selection: $propina) {
-                    ForEach(propinas, id: \.self) {
-                        Text($0, format: .percent)
+                        // valor    2-3-4-5-......
+                        // posicion 0-1-2-3-4-5-6-
                     }
                 }
-                // usaremos este picker con selecciones cortas y una lista pequeña
-                       .pickerStyle(.segmented)
-            }
 
-            Section {
-                Text(totalPorPersona, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section {
+                    Text("Cuanta propina deseas dejar?")
+                    Picker("Porcentaje de propina",
+                           selection: $propina) {
+                        ForEach(propinas, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    // usaremos este picker con selecciones cortas y una lista pequeña
+                           .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Text(totalPorPersona, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("Dividir la propina")
+            .toolbar {
+                if estaElFocusEnValorTotal {
+                    Button("Listo") {
+                        estaElFocusEnValorTotal = false
+                    }
+                }
             }
         }
     }
@@ -78,3 +93,5 @@ struct ContentView: View {
 // concepto que se refiere a propiedades culturales de la region en la que se ejecuta la app
 // argentina -  Peso Argentino - izq/der - yyyy/dd/mm - 1,000
 // usa          USD - izq/der - mm/dd/yyyy - 1.000
+
+// Focus en FrontEnd significa el objeto de UI que está respondiendo al usuario
