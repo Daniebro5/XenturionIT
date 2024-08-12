25e68031ba21fd9e64521b42aa4cb19fa67af116
Danni Brito <danni.brito.-nd@disney.com>
2024-08-12 10:21:48 -0500

    Update ContentView with stateful button logic

--- BancoPacifico/WeSplit/WeSplit/ContentView.swift
+++ BancoPacifico/WeSplit/WeSplit/ContentView.swift
@@ -2,16 +2,17 @@ import SwiftUI
 
 struct ContentView: View {
 
-    var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello, World!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-        }
+    // Las vistas son una funcion de su estado
+    // es decir es la manera en que la interfaz de usuario luce, dependiendo del estado de la aplicacion
 
+    // state me permite q esta variable sea mutabvle (algo que no se puede en las estructuras)
+    // State especificamente diseñado para propiedades simples que serán almacenadas en una sola vista
+    @State private var tapCount = 0
+
+    var body: some View {
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+        }
     }
 }
 
@@ -24,3 +25,7 @@ struct ContentView: View {
     ContentView()
         .preferredColorScheme(.dark)
 }
+
+// struct vs clase
+// la estructura es inmutable
+// let a = ContentView()
