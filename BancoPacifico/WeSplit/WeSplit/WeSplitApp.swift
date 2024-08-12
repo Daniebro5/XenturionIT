2e5da9ba6048010ff79399f3ebc09db20f016211
Danni Brito <danni.brito.-nd@disney.com>
2024-08-12 09:46:25 -0500

    Update WeSplitApp with security comment

--- BancoPacifico/WeSplit/WeSplit/WeSplitApp.swift
+++ BancoPacifico/WeSplit/WeSplit/WeSplitApp.swift
@@ -12,3 +12,4 @@ struct WeSplitApp: App {
 // si queremos que algo viva por todo el ciclo de la app, debemos ponerlo aquí
 // por ejemplo analytics, o registrar la app para notificaciones
 // aqui chequeriamos si existe una sesion del usuario, la capa de networking debe tener aditivos como para manejar el bearer token si este es usado.
+// aqui registrariamos la seguridad de nuestra aplicacion
