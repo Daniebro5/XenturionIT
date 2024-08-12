c03eafa8ab5a18e6f9b99ef493bc49148549b086
Danni Brito <danni.brito.-nd@disney.com>
2024-08-12 09:59:08 -0500

    Update ContentView with NavigationStack and title

--- BancoPacifico/WeSplit/WeSplit/ContentView.swift
+++ BancoPacifico/WeSplit/WeSplit/ContentView.swift
@@ -3,22 +3,15 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        Form {
-            // agrupen items relacionados
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello, World!")
+                }
             }
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-
+            .navigationTitle("SwiftUI")
         }
+
     }
 }
 
