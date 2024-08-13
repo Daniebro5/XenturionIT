9f65ae9b087ad48afcdbc3f574354287e1a9dcbb
Danni Brito <danni.brito.-nd@disney.com>
2024-08-13 09:33:19 -0500

    Update ContentView with user list and sorting

--- BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
+++ BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
@@ -1,17 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+
+    // Sobrecarga de operadores
+    // left-right hand side (user1) < (user2)
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+
+    // comparable tambien nos da el operador >, solo hace flip del booleano del operador <
 }
