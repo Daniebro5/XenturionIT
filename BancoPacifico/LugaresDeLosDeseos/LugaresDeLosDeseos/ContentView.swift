9ca35a965ae89729dedd961d279e63c1bb30f444
Danni Brito <danni.brito.-nd@disney.com>
2024-08-13 09:59:02 -0500

    Add loading states and views to ContentView

--- BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
+++ BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
@@ -1,23 +1,41 @@
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Button("Read and Write") {
-            let data = Data("Mensaje de prueba".utf8)
-            let url = URL.documentsDirectory.appending(path: "mensaje.txt")
+enum EstadoDeCarga {
+    case cargando, completado, fallido
+}
 
-            do {
-                try data.write(to: url,
-                               options: [
-                                .atomic,
-                                    .completeFileProtection
-                               ])
-                let input = try String(contentsOf: url)
-                print(input)
-            } catch {
-                print(error.localizedDescription)
-            }
+struct ContentView: View {
+
+    @State private var estadoDeCarga = EstadoDeCarga.fallido
+
+    var body: some View {
+        // ventaja: Switch utiliza el compilador para saber si todos los cases han sido cubiertos, es decir si en un futuro añadimos un nuevo case, nos va a dar un error para añadirlo aquí
+        switch estadoDeCarga {
+        case .cargando:
+            CargandoView()
+        case .completado:
+            CompletadoView()
+        case .fallido:
+            FallidoView()
         }
+    }
+}
+
+struct CargandoView: View {
+    var body: some View {
+        Text("cargando")
+    }
+}
+
+struct CompletadoView: View {
+    var body: some View {
+        Text("completado")
+    }
+}
+
+struct FallidoView: View {
+    var body: some View {
+        Text("fallido")
     }
 }
 
