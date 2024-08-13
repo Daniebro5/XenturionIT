4e7e2239982fc2a5e34fe5fa32a3f841c541e183
Danni Brito <danni.brito.-nd@disney.com>
2024-08-13 10:41:19 -0500

    Update ContentView with MapKit and camera control

--- BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
+++ BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView.swift
@@ -1,41 +1,83 @@
+import MapKit
 import SwiftUI
-
-enum EstadoDeCarga {
-    case cargando, completado, fallido
-}
 
 struct ContentView: View {
 
-    @State private var estadoDeCarga = EstadoDeCarga.fallido
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: -2.1891,
+                longitude: -79.8899),
+            // span es que tanto de su alrededor se ve del punto inicial
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01))
+    )
 
     var body: some View {
-        // ventaja: Switch utiliza el compilador para saber si todos los cases han sido cubiertos, es decir si en un futuro añadimos un nuevo case, nos va a dar un error para añadirlo aquí
-        switch estadoDeCarga {
-        case .cargando:
-            CargandoView()
-        case .completado:
-            CompletadoView()
-        case .fallido:
-            FallidoView()
+        // gestos
+        // option nos da el gesto del pinch o dos dedos para hacer zoom
+        // option + shift nos da el gesto de rotacion o inlcinacion de mapa con dos dedos
+        // doble tap nos da el zoom
+//        Map()
+//        // style imaginery es satelite
+//            .mapStyle(.imagery)
+        // hybrid combina ambos
+        // hybris con elevation nos da relieve
+
+        // le decimos como el usuario puede interactuar
+        // vacio significa que el mapa es estatico
+//        Map(interactionModes: [])
+//            .mapStyle(.hybrid(elevation: .realistic))
+
+        VStack {
+            Map(position: $position)
+                .onMapCameraChange { context in
+                    print(context.region)
+                }
+
+
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(
+                                latitude: 48.8566,
+                                longitude: 2.3522),
+                            // span es que tanto de su alrededor se ve del punto inicial
+                            span: MKCoordinateSpan(
+                                latitudeDelta: 0.01,
+                                longitudeDelta: 0.01))
+                        )
+                }
+
+                Button("Tokio") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(
+                                latitude: 35.6897,
+                                longitude: 139.6922),
+                            // span es que tanto de su alrededor se ve del punto inicial
+                            span: MKCoordinateSpan(
+                                latitudeDelta: 0.01,
+                                longitudeDelta: 0.01))
+                        )
+                }
+
+                Button("Quito") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(
+                                latitude: 0.0,
+                                longitude: -78.4810),
+                            // span es que tanto de su alrededor se ve del punto inicial
+                            span: MKCoordinateSpan(
+                                latitudeDelta: 0.01,
+                                longitudeDelta: 0.01))
+                        )
+                }
+            }
         }
-    }
-}
-
-struct CargandoView: View {
-    var body: some View {
-        Text("cargando")
-    }
-}
-
-struct CompletadoView: View {
-    var body: some View {
-        Text("completado")
-    }
-}
-
-struct FallidoView: View {
-    var body: some View {
-        Text("fallido")
     }
 }
 
