f1b49bb16b87a089131d76be1f44ce4dc346a064
Danni Brito <danni.brito.-nd@disney.com>
2024-08-14 11:43:46 -0500

    Add Creacionales and Introduccion playgrounds

---
+++ BancoPacifico/PatronesDeDiseño/Introduccion.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+/* Patrones de Diseño
+soluciones probadas a problemas comunes en el proceso de desarrollo de software.
+ nos dicen como crear o interactuar con objetos de la manera mas eficiente posible.
+
+ son formas de organizar nuestro codigo
+
+ 1. patrones creacionales
+    como recetas de cocina que nos ayudan a preparar distintos tipos de alimentos con ingredientes correctos, estos nos dicen como crear objetos
+    a. Singleton (una olla para todas las sopas)
+    b. Factory Method (una maquina que hace distintos tipos de cafe)
+    c. Abstract Factory (una fabrica que hace combos completos de comida rapida)
+    d. Builder (Hacer la pizza paso a paso)
+    e. Prototype (Clonar una planta a partir de una hoja)
+
+ 2. patrones estructurales
+    son como los planos de una casa para q esta sea solida y organizada, que nos permite saber si una habitacion puede estar al lado de otra
+    a. Adapter (un adaptador que me permite cargar mi celu en Argentina)
+    b. Composite (Arbol genealogico, la jerarquia de vistas de Swiftui)
+    c. Decorator (decorar un arbol de navidad)
+    d. proxy (representante que actua en tu lugar)
+    e. Facade (Fachada, una interfaz de botones que oculta toda una fabrica)
+
+3. patrones de comportamiento
+    como las reglas de transito, definen como los diferentes obketos interactuan entre si, se comunican y coordinan
+    a. Observer (suscribirse a un canal de youtube)
+    b. Strategy (escoger una estrategia en un juego de aventura)
+    c. command (dar ordenes a mi peloton)
+    d. State (cambiar de modo un aparato electronico, el estado de las vistas en swiftUI)
+    e. Iterator (para pasar pagina en un libro)
+
+
+ */
