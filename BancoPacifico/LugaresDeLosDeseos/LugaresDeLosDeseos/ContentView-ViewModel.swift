7c2d1774c5679c75eb301dd92a3c3bcaf6960066
Danni Brito <danni.brito.-nd@disney.com>
2024-08-14 10:56:18 -0500

    Add biometric auth and refactor ContentView

--- BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView-ViewModel.swift
+++ BancoPacifico/LugaresDeLosDeseos/LugaresDeLosDeseos/ContentView-ViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -10,8 +11,20 @@ extension ContentView {
 
         // con private(set) nos ayudamos a identificar las partes de nuestro codigo que estan escribiendo en esta variable, algo que queremos evitar, xq solo este propio viewmodel debe ser capaz de modificar sus variables
         // leerlas está bien para actualizar la vista, por lo que el GET sigue siendo publico
-        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
 
         // Sintaxis Swifty
         // siempre pensemos en como se va a llamar la funcion para ser lo mas declarativos posible.... .addLocation(at: )
@@ -22,6 +35,7 @@ extension ContentView {
                                        latitude: point.latitude,
                                        longitude: point.longitude)
             locations.append(newLocation)
+            save()
         }
 
         // siempre pensemos en como se va a llamar la funcion para ser lo mas declarativos posible.... .update(location: )
@@ -30,6 +44,36 @@ extension ContentView {
 
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+            }
+            save()
+        }
+
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+
+        // Firma () -> Void ó Void -> Void ó () -> ()
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                // esta razon se usa para el touchID, la string en el archivo de propiedades del proyecto se usa para el FaceID
+                let reason = "Please authenticate yourself to unlock your places."
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authError in
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
             }
         }
 
