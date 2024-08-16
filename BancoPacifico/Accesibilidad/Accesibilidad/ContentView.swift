56676b650078de77e08eb0ae88856888f1dfba2f
Danni Brito <danni.brito.-nd@disney.com>
2024-08-16 12:37:43 -0500

    Update ContentView with button in SwiftUI

--- BancoPacifico/Accesibilidad/Accesibilidad/ContentView.swift
+++ BancoPacifico/Accesibilidad/Accesibilidad/ContentView.swift
@@ -1,16 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var volumen: Double = 50
-    var body: some View {
-        VStack {
-            Slider(value: $volumen, in: 0 ... 100, step: 1)
-                .padding()
-                .accessibilityLabel(Text("Volumen"))
-                .accessibilityValue(Text("\(Int(volumen)) porciento"))
 
-            Text("Volumen: \(Int(volumen))%")
-        }
+    var body: some View {
+        Button("Hello, SwiftUI", action: {})
+            .padding()
+            .foregroundColor(.white)
+            .background(.black)
+            .clipShape(.capsule)
     }
 }
 
