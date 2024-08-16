519dc685e2fa738a252490d1832969ad1d281207
Danni Brito <danni.brito.-nd@disney.com>
2024-08-16 12:24:47 -0500

    Update ContentView with new image and accessibility

--- BancoPacifico/Accesibilidad/Accesibilidad/ContentView.swift
+++ BancoPacifico/Accesibilidad/Accesibilidad/ContentView.swift
@@ -1,22 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isDarkMode = false
     var body: some View {
-        VStack {
-            Button(action: {
-                isDarkMode.toggle()
-            }, label: {
-                Image(systemName: isDarkMode ? "sun.max.fill" : "moon.fill")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(isDarkMode ? Color.black : Color.white)
-                    .foregroundStyle(isDarkMode ? .white : .black)
-                    .clipShape(.capsule)
-                    .ignoresSafeArea()
-            })
-            .accessibilityLabel(Text("Cambia el dark mode. Actualmente, dark mode esta \(isDarkMode ? "activado" : "desactivado")"))
-        }
-
+        Image(systemName: "photo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+            .accessibilityLabel(Text("Paisaje de montaña"))
+            .accessibilityHint(Text("Un bello paisaje de una montaña en un atardecer"))
     }
 }
 
